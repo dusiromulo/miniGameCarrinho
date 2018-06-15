fbce659cf65380a695710537a1135e4cb49ff544
Asderek <pinheiro.lucasn@gmail.com>
2018-06-14 22:06:56 -0300

    Modularizacao
    Modularizado os carros

---
+++ car.lua
@@ -0,0 +1,50 @@
+car = {}
+carPositions = {}
+local w, h
+
+function car.newCar(posIndex, posY, sprite)
+  
+  w, h = love.graphics.getDimensions()
+  local carWidth = 44
+
+  carPositions[1] = 0.17*w - carWidth
+  carPositions[2] = 0.17*w - carWidth + 0.33*w
+  carPositions[3] = 0.17*w - carWidth + 0.66*w
+    
+  obj = {
+      x = carPositions[posIndex],
+      y = posY,
+      image = love.graphics.newImage(sprite),
+      
+      draw = function (car)
+        love.graphics.draw( car.image, car.x, car.y)
+      end,
+      
+      move = function(car, direction)
+        if (direction == "left") then
+          if (car.x == carPositions[2]) then
+            car.x = carPositions[1]
+          elseif (car.x == carPositions[3]) then
+            car.x = carPositions[2]
+          else
+          end
+        else
+          if (car.x == carPositions[1]) then
+            car.x = carPositions[2]
+          elseif (car.x == carPositions[2]) then
+            car.x = carPositions[3]
+          else
+          end
+        end
+      end,
+      
+      update = function(car)
+      end    
+      
+  }
+  
+  return obj
+  
+end
+
+return car
--- main.lua
+++ main.lua
@@ -1,23 +1,16 @@
+CAR_MODULE = require 'car'
 local w, h
 local stripeWidth = 9
-local carWidth = 44
 local cont = 0
-local updateStep = 50
-carPositions = {}
+local updateStep = 20
 stripes = {}
 cars = {}
 
-function love.load()
-  love.window.setMode(1280,720)
-  love.graphics.setBackgroundColor(1,1,1)
-  w, h = love.graphics.getDimensions()
+local function loadStripes()
   
-  carPositions[1] = 0.17*w - carWidth
-  carPositions[2] = 0.17*w - carWidth + 0.33*w
-  carPositions[3] = 0.17*w - carWidth + 0.66*w
   local darkImg = love.graphics.newImage("images/dark.png")
   local lightImg = love.graphics.newImage("images/light.png")
-  --<Left Stripes>
+  
   for i=1,20 do
     stripes[#stripes+1] = {
                               x = 0,
@@ -77,87 +70,102 @@ function love.load()
   end
   --</Right Stripes>
   
-  --<Cars>
-  for i=0,1 do
-    cars[#cars+1] = {
-                        x = carPositions[i+1],
-                        y = h*0.8
-                    }
-    if (math.fmod(i,2) == 0) then
-      cars[#cars].imagem = love.graphics.newImage("images/car1.png")
-    else
-      cars[#cars].imagem = love.graphics.newImage("images/car2.png")
+  stripes.update = function ()
+      for i=1,#stripes do
+        stripes[i].y = stripes[i].y + h/40
+        if (stripes[i].y > h) then
+          stripes[i].y = -h/40
+        end
+      end
     end
-    
-  end
-  --</Cars>
+  
   
 end
 
-function love.update()
-  cont = cont + 1
-  if (love.keyboard.isDown("w")) then
-    for i=1,#stripes do
-      stripes[i].y = stripes[i].y + h/40
-      if (stripes[i].y > h) then
-        stripes[i].y = -h/40
-      end
+local function loadCars(numCars)
+  if numCars > 3 then
+    numCars = 3
+  end
+  for i=1,numCars do    
+    if (math.fmod(i,2) == 0) then
+      cars[#cars+1] = CAR_MODULE.newCar(i,h*0.8, "images/car1.png")
+    else
+      cars[#cars+1] = CAR_MODULE.newCar(i,h*0.8, "images/car2.png")
+    end
+  end  
+  
+end
+
+
+
+local function checkMsgs()
+  
+  if #cars>0  then
+    if (love.keyboard.isDown("a")) then
+      cars[1]:move("left")
+    elseif (love.keyboard.isDown("d")) then
+      cars[1]:move("right")
+    else
     end
   end
-  
-  if(cont > updateStep) then
-    cont = 0
-    
-    --<control Car1>
-    if (love.keyboard.isDown("d")) then
-        if (cars[1].x == carPositions[1]) then
-          cars[1].x = carPositions[2]
-        elseif (cars[1].x == carPositions[2]) then
-          cars[1].x = carPositions[3]
-        else
-        end
-    end
-  
-    if (love.keyboard.isDown("a")) then
-        if (cars[1].x == carPositions[2]) then
-          cars[1].x = carPositions[1]
-        elseif (cars[1].x == carPositions[3]) then
-          cars[1].x = carPositions[2]
-        else
-        end
-    end
-    --</control Car1>
-    
-    --<control Car2>
-    if (love.keyboard.isDown("e")) then
-        if (cars[2].x == carPositions[1]) then
-          cars[2].x = carPositions[2]
-        elseif (cars[2].x == carPositions[2]) then
-          cars[2].x = carPositions[3]
-        else
-        end
-    end
-  
+
+  if #cars>1  then
     if (love.keyboard.isDown("q")) then
-        if (cars[2].x == carPositions[2]) then
-          cars[2].x = carPositions[1]
-        elseif (cars[2].x == carPositions[3]) then
-          cars[2].x = carPositions[2]
-        else
-        end
+      cars[2]:move("left")
+    elseif (love.keyboard.isDown("e")) then
+      cars[2]:move("right")
+    else
     end
-    --</control Car2>
-  
+  end
+
+  if #cars>2  then
+    if (love.keyboard.isDown("z")) then
+      cars[3]:move("left")
+    elseif (love.keyboard.isDown("c")) then
+      cars[3]:move("right")
+    else
+    end
   end
 end
+
+
+function love.load()
+  love.window.setMode(1280,720)
+  love.graphics.setBackgroundColor(1,1,1)
+  w, h = love.graphics.getDimensions()
+  
+  loadStripes()
+  loadCars(2)
+  
+  --<Create Cars>
+ 
+  --</Create Cars>
+  
+end
+
+
+
+function love.update()
+  cont = cont + 1
+  stripes.update()
+  
+  if(cont > updateStep) then
+    cont = 0   
+    checkMsgs()
+    for i,v in pairs(cars) do
+      cars[i]:update()
+    end
+  end
+end
+
 
 function love.draw()
   for i=1,#stripes do
       love.graphics.draw( stripes[i].imagem, stripes[i].x, stripes[i].y)
   end
   
-  for i=1,#cars do
-      love.graphics.draw( cars[i].imagem, cars[i].x, cars[i].y)
-  end
+  for i,v in pairs(cars) do
+      cars[i]:draw()
+    end
 end
   
