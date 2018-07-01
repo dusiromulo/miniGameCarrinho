5c3fe459f2e8dd810d477b8a494c782b84d781e3
Rômulo Dusi <dusiromulo@gmail.com>
2018-06-30 22:08:10 -0300

    node changes

--- node/mini_game_carrinho_protocol.lua
+++ node/mini_game_carrinho_protocol.lua
@@ -5,7 +5,7 @@ local led2 = 6
 local sw1 = 1
 local sw2 = 2
 
-local playerName = "Breno"
+local playerName = "Breno420"
 local playerRand = math.floor(math.random(1, 99999))
 local playerNumber = ""
 
@@ -14,7 +14,7 @@ local playTopicName = "new_player" .. playerPosTopic
 local isPlaying = false
 
 local maxTimeTriggerTiro = 200000 -- 200 ms!
-local buttonBounceLimit = 100000 -- 100 ms!
+local buttonBounceLimit = 300000 -- 100 ms!
 local lastBtn1Press, lastBtn2Press = 0, 0
 
 gpio.mode(sw1, gpio.INPUT, gpio.PULLUP)
@@ -25,35 +25,36 @@ gpio.mode(led2, gpio.OUTPUT)
 
 local function moveLeft(_, time)
     print("pressionou chave 1")
-    
-    if time - lastBtn2Press > buttonBounceLimit then
-        if isPlaying then
-            if time - lastBtn2Press < maxTimeTriggerTiro then
-                msgr.sendMessage("ambos", playTopicName)
-            else 
-                msgr.sendMessage("esq", playTopicName)
-            end
-        else
-            msgr.sendMessage(playerName..playerRand, playTopicName)
-        end
-    end
+    gpio.trig(sw1)
     lastBtn1Press = time
+    if isPlaying then
+        if time - lastBtn2Press < maxTimeTriggerTiro then
+            msgr.sendMessage("ambos", playTopicName)
+        else 
+            msgr.sendMessage("esq", playTopicName)
+        end
+    else
+        msgr.sendMessage(playerName..playerRand, playTopicName)
+    end
+    tmr.delay(buttonBounceLimit)
+    gpio.trig(sw1, "down", moveLeft)
 end
 
 local function moveRight(_, time)
     print("pressionou chave 2")
-    if time - lastBtn2Press > buttonBounceLimit then
-        if isPlaying then
-            if time - lastBtn1Press < maxTimeTriggerTiro then
-                msgr.sendMessage("ambos", playTopicName)
-            else 
-                msgr.sendMessage("dir", playTopicName)
-            end
-        else
-            msgr.sendMessage(playerName..playerRand, playTopicName)
-        end
-    end
+    gpio.trig(sw2)
     lastBtn2Press = time
+    if isPlaying then
+        if time - lastBtn1Press < maxTimeTriggerTiro then
+            msgr.sendMessage("ambos", playTopicName)
+        else 
+            msgr.sendMessage("dir", playTopicName)
+        end
+    else
+        msgr.sendMessage(playerName..playerRand, playTopicName)
+    end
+    tmr.delay(buttonBounceLimit)
+    gpio.trig(sw2, "down", moveRight)
 end
 
 local function mensagemRecebida (mensagem)
--- node/wificonnect.lua
+++ node/wificonnect.lua
@@ -4,8 +4,8 @@ local function wifi_connected_callback(iptable)
 end
 
 wificonf = {
-  ssid = "AndroidAP",
-  pwd = "asdf1234",
+  ssid = "Breno",
+  pwd = "M@gr042o",
   got_ip_cb = wifi_connected_callback,
   save = false,
 }
