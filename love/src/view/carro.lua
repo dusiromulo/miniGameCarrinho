8a849523aba4f93edfd3ea605cdbb009b9c8b194
Rômulo Dusi <dusiromulo@gmail.com>
2018-07-02 23:15:28 -0300

    bug fix? [3]

--- love/src/view/carro.lua
+++ love/src/view/carro.lua
@@ -23,6 +23,12 @@ local carro = {
 			obj.controle:update()
 		end
 	end,
+	createControle = function (obj, id, channel)
+		local callbackEsq = function() obj:moveEsq() end
+		local callbackDir = function() obj:moveDir() end
+		local callbackBoth = function() obj:both() end
+		obj.controle = controle.cria(id, channel, callbackEsq, callbackDir, callbackBoth)
+	end,
 	draw = function (obj)
 		love.graphics.setColor(255, 255, 255)
 		love.graphics.draw(obj.imagem, obj.x, obj.y)
@@ -71,20 +77,16 @@ function carro.cria(id, channel, startX, moveOffsetX, fixedY, imagem, callbackRe
 	local carImg = love.graphics.newImage(imagem)
 	local carWidth = carImg:getWidth()
 
-	local callbackEsq = function() car:moveEsq() end
-	local callbackDir = function() car:moveDir() end
-	local callbackBoth = function() car:both() end
-	
 	car = {
 		callbackRestart = callbackRestart,
 		x = startX - carWidth/2,
 		y = fixedY,
 		carPositions = {startX - moveOffsetX - carWidth/2, startX - carWidth/2, startX + moveOffsetX - carWidth/2},
-		imagem = carImg,
-		controle = controle.cria(id, channel, callbackEsq, callbackDir, callbackBoth)
+		imagem = carImg
 	}
 
 	setmetatable(car, mt)
+	car:createControle(id, channel)
 	return car
 end
 
